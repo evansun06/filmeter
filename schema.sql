b805c2b4138eea05a61bc847d01170af424dc1e7
Evan Sun <evansun06@outlook.com>
2025-05-21 12:50:30 -0600

    Added SQL database schema for Postgres Server

---
+++ schema.sql
@@ -0,0 +1,31 @@
+-- Postgres SQL schema for RESTFUL API
+
+-- USERS
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    hashed_password TEXT NOT NULL
+);
+
+-- MOVIES
+CREATE TABLE movies (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    director TEXT,
+    release_date INT
+);
+
+
+-- REVIEWS
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    movie_id INT NOT NULL,
+    letter_grade TEXT,
+    written_review TEXT,
+
+    -- references to other tables
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+	FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE
+);
